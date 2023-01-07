1240826d718640bad0f9577ace65021e8ed3028b
rubylo718 <rubylo718@gmail.com>
2023-01-07 13:21:32 +0800

    refactor: refactor routes

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
-const { authenticated, authUser } = require('../middleware/auth')
+const { authenticated, authUser, authAdmin } = require('../middleware/auth')
+const { errorHandler } = require('../middleware/error-handler')
 
 const userController = require('../controllers/user-controller')
 const recordController = require('../controllers/record-controller')
 const calendarController = require('../controllers/calendar-controller')
-const { errorHandler } = require('../middleware/error-handler')
-
-router.get('/', authenticated, authUser, (req, res) => {
-  res.send('hello world')
-})
 
 router.get('/users/currentUser', authenticated, userController.getCurrentUser)
 router.post('/users/signin', userController.signIn)
 router.put('/users/edit', authenticated, authUser, userController.editUser)
+
 router.get('/records/rawRecords', authenticated, authUser, recordController.getUserRecords)
+
 router.post('/check',authenticated, authUser, recordController.addRecord)
-router.put('/calendar', calendarController.updateCalendar)
+
+router.put('/calendar',authenticated, authAdmin, calendarController.updateCalendar)
 
 router.use('/', errorHandler)
 
