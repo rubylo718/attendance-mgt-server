2b490b80f5b40356213b17c9a27073dafa017af7
rubylo718 <rubylo718@gmail.com>
2023-01-09 13:31:04 +0800

    feat: add swagger to generate API docs, remove /apiDocs

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ const userController = require('../controllers/user-controller')
 const recordController = require('../controllers/record-controller')
 const calendarController = require('../controllers/calendar-controller')
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger_output.json')
+
 router.get('/users/currentUser', authenticated, userController.getCurrentUser)
 router.post('/users/signin', userController.signIn)
 router.put('/users/edit', authenticated, authUser, userController.editUser)
@@ -17,6 +20,8 @@ router.get('/records/rawRecords', authenticated, authUser, recordController.getU
 router.post('/check',authenticated, authUser, recordController.addRecord)
 
 router.put('/calendar',authenticated, authAdmin, calendarController.updateCalendar)
+
+router.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 router.use('/', errorHandler)
 
---
+++ swagger.js
@@ -0,0 +1,6 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const outputFile = './swagger_output.json'
+const endPointFiles = ['./app.js']
+
+swaggerAutogen(outputFile, endPointFiles)
